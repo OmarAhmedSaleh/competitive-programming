c3d91ee4adce4e8b1c2807feae94f59f7f20cc83
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-03-01 19:39:24 +0200

    Create 458 - The Decoder.cpp

---
+++ UVA/458 - The Decoder.cpp
@@ -0,0 +1,56 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <memory.h>
+#include <cassert>
+#include <complex>
+
+using namespace std;
+int dx[8]={1,-1,0,0,1,-1,1,-1};
+int dy[8]={0,0,-1,1,1,-1,-1,1};
+const double EPS=1e-8;
+const int N=100005;
+typedef complex<double> point;
+#define X real()
+#define Y imag()
+#define cp(a,b) (conj(a)*b).imag()
+#define dp(a,b) (conj(a)*b).real()
+#define lengthsq(a)    dp(a,a)
+/*
+ -- Valid
+ -- const (10^9>sz)
+ -- array index
+ -- less or more , check if
+ -- even or odd
+ -- inequality
+ */
+string s;
+
+int main(){
+    while(getline(cin,s)){
+        int n=(int)s.size();
+        for(int i=0;i<n;i++){
+            s[i]-=7;
+        }
+        cout<<s<<endl;
+    }
+    
+    return 0;//rev Ab steps
+}
