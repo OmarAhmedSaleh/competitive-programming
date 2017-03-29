3c438eb1b2cd073b9ad9242ff1f1948c1f921e23
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-03-30 01:35:26 +0200

    Create 1684. Jack's Last Word.cpp

---
+++ TJU/1684. Jack's Last Word.cpp
@@ -0,0 +1,87 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <memory.h>
+#include <cassert>
+#include <complex>
+
+using namespace std;
+int dx[8]={1,-1,0,0,1,-1,1,-1};
+int dy[8]={0,0,-1,1,1,-1,-1,1};
+
+
+string T,P;
+vector<int> ans;
+
+int fil[150004],n,m,idx,sz;
+void ff(){
+    fil[0]=0;
+    int k=0;
+    for(int i=1;i<n;i++){
+        while(k>0 && T[k]!=T[i]){k=fil[k-1];}
+        if(T[i]==T[k]){
+           k++;
+        }
+        fil[i]=k;
+       
+        
+    }
+
+}
+bool f;
+bool sp[75001];
+int main() {
+    cin>>T;
+    sz=(int)T.size();
+    cin>>P;
+    m=(int)P.size();
+    T+='#';
+    T+=P;
+    n=(int)T.size();
+    ff();
+    ans.clear();
+    int pos=n-1;
+    while(fil[pos]){
+        pos-=fil[pos];
+        ans.push_back(pos-sz);
+        if(T[pos]=='#'){f=1;break;}
+
+    }
+    if(!f){
+        printf("Yes\n");
+    }else{
+        if(ans.size()){
+            reverse(ans.begin(),ans.end());
+        }
+            printf("No\n");
+        for(int i=1;i<ans.size();i++){
+            sp[ans[i]]=1;
+        }
+        for(int i=0;i<P.size();i++){
+            if(sp[i]){printf(" ");}
+            printf("%c",P[i]);
+            
+        }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
