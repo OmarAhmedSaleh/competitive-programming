f2c36f59d1f921d66416800e542bb7b0ba3787c7
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-04-03 16:41:16 +0200

    Create 10165 - Stone Game.cpp

---
+++ UVA/10165 - Stone Game.cpp
@@ -0,0 +1,49 @@
+
+/*
+ try all possible cuts , find area of two polygons
+ */
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <memory.h>
+#include <cassert>
+#include <complex>
+
+using namespace std;
+int dx[8]={1,-1,0,0,1,-1,1,-1};
+int dy[8]={0,0,-1,1,1,-1,-1,1};
+int n;
+long long a;
+int main(){
+    while(scanf("%d",&n)==1){
+        if(!n){break;}
+        long long b=0;
+        for(int i=0;i<n;i++){
+            scanf("%lld",&a);
+            b^=a;
+        }
+        if(!b){
+            puts("No");
+        }else{
+            puts("Yes");
+        }
+    }
+    return 0;
+}
