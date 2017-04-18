4f57e30eaaed32475366a12fa5db59fcca709814
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-04-18 19:28:47 +0200

    Create 7150 - Amalgamated Artichokes.cpp

---
+++ ACM-ICPC-Live-Archive/7150 - Amalgamated Artichokes.cpp
@@ -0,0 +1,53 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <memory.h>
+#include <cassert>
+#include <complex>
+
+using namespace std;
+int dx[8]={1,-1,0,0,1,-1,1,-1};
+int dy[8]={0,0,-1,1,1,-1,-1,1};
+
+/*
+ -- Valid
+ -- const (10^9>sz)
+ -- array index
+ -- less or more , check if
+ -- even or odd
+ -- inequality
+ */
+
+int a, b, c, d,p,n;
+double sofar,ans,mm;
+int main(){
+    while(scanf("%d",&p)!=EOF){
+        scanf("%d%d%d%d%d",&a,&b,&c,&d,&n);
+        ans=sofar=0;
+        for(int i=1;i<=n;i++){
+        mm=p*(sin(a*i+b)+cos(c*i+d)+2);
+            sofar=max(sofar,mm);
+            ans=max(ans,sofar-mm);
+        }
+        printf("%.6lf\n",ans);
+    }
+    
+    return 0;
+}
