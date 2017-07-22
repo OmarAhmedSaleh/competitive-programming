02959747cb9e9275fa17e7981c38bb65e58aae69
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-07-23 01:42:06 +0200

    Update CF359-D2-D.cpp

--- codeforces/CF359-D2-D.cpp
+++ codeforces/CF359-D2-D.cpp
@@ -1,3 +1,14 @@
+/*
+ Quite simple note: if the pair (l, r) satisfies the condition 1 from the statements,
+ then min(l, r) = GCD(l, r), where min(l, r) is smallest number ai from the segment (l, r) 
+ and GCD(l, r) is a GCD of all numbers from the segment (l, r). 
+ Calculate some data structure that will allow us to respond quickly to requests GCD(l, r) and min(l, r). 
+  For example, you can use Sparce Table. Solutuions, that uses segment tree, is too slow. 
+  So I think, you should use Sparce Table. So, now our task quite simple. 
+  Let's use binary search to find greatest value of r - l:
+ */
+
+
 #include <cstring>
 #include <vector>
 #include <list>
