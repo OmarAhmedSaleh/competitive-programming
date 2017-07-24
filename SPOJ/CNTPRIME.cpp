94fb0ea899c8fd06a2093c0e42c9331df424802e
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-07-24 06:51:23 +0200

    Create CNTPRIME.cpp

---
+++ SPOJ/CNTPRIME.cpp
@@ -0,0 +1,127 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <memory.h>
+#include <cassert>
+#include <complex>
+
+using namespace std;
+//const long long OO=4e18;
+//const int oo=1e9;
+//int dx[8]={1,-1,0,0,1,-1,1,-1};
+//int dy[8]={0,0,-1,1,1,-1,-1,1};
+const int MAX=10000<<2;
+int m;
+struct node{
+    int lazy,l,r,sum;
+    node(){
+        lazy=sum=0;
+    }
+};
+node st[MAX];
+bool isc[1000001];
+void push(int idx){
+    st[idx].sum=(!isc[st[idx].lazy])*(st[idx].r-st[idx].l+1);
+    if((idx<<1)<MAX){
+        st[idx<<1].lazy=st[idx].lazy;
+    }
+    if((idx<<1)+1<MAX){
+        st[(idx<<1)+1].lazy=st[idx].lazy;
+    }
+    st[idx].lazy=0;
+}
+void pull(node &a,node&b,node&c){
+    c.sum=a.sum+b.sum;
+}
+void build(int idx,int l, int r){
+    st[idx].l=l,st[idx].sum=0,st[idx].r=r,st[idx].lazy=0;
+    if(l==r){
+        scanf("%d",&m);
+        st[idx].sum=!isc[m];
+        return ;
+    }
+    build(idx<<1,l,(l+r)/2);
+    build((idx<<1)+1,(l+r)/2+1,r);
+    pull(st[idx<<1],st[(idx<<1)+1],st[idx]);
+}
+int s,e,v;
+void modi(int idx,int l, int r){
+    if(st[idx].lazy){
+        push(idx);
+    }
+    if(l>e||r<s){
+        return ;
+    }
+    if(l>=s&&r<=e){
+        st[idx].lazy=v;
+        push(idx);
+        return ;
+    }
+    modi(idx<<1,l,(l+r)/2);
+    modi((idx<<1)+1,(l+r)/2+1,r);
+    pull(st[idx<<1],st[(idx<<1)+1],st[idx]);
+}
+node get(int idx,int l, int r){
+    node ret;
+    if(st[idx].lazy){
+        push(idx);
+    }
+    if(l>e||r<s){
+        return ret;
+    }
+    if(l>=s&&r<=e){
+        return st[idx];
+    }
+    node a=get(idx<<1,l,(l+r)/2);
+    node b=get((idx<<1)+1,(l+r)/2+1,r);
+    pull(a,b,ret);
+    return ret;
+}
+int n , q, tc ,ty;
+int main(){
+    memset(isc,0,sizeof(isc));
+    isc[0]=isc[1]=1;
+    for(int i=4;i<=1e6;i+=2){
+        isc[i]=1;
+    }
+    for(int i=3;i<=1e6;i+=2){
+        if(isc[i]){continue;}
+        for(int j=i+i;j<=1e6;j+=i){
+            isc[j]=1;
+        }
+    }
+    scanf("%d",&tc);
+    for(int tt=1;tt<=tc;tt++){
+        scanf("%d%d",&n,&q);
+        build(1,0,n-1);
+        printf("Case %d:\n",tt);
+        while(q-->0){
+            scanf("%d%d%d",&ty,&s,&e);
+            s--;e--;
+            if(ty){
+                printf("%d\n",get(1,0,n-1).sum);
+            }else{
+                scanf("%d",&v);
+                modi(1,0,n-1);
+            }
+        }
+    }
+    return 0;
+}
