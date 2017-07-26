b25cc06e3130adfc29773fc1df0f5d7c4eb26b4c
OmarAhmedSaleh <omar.sa006@gmail.com>
2017-07-26 18:36:36 +0200

    Update CF168-D2-D.cpp

--- codeforces/CF168-D2-D.cpp
+++ codeforces/CF168-D2-D.cpp
@@ -30,25 +30,39 @@ int dx[8]={1,-1,0,0,1,-1,1,-1};
 int dy[8]={0,0,-1,1,1,-1,-1,1};
 */
 int n , l , k , a[200],p[200];
-double memo[200][401][201];
-bool seen[200][401][201];
+double memo[200][201][201];
+bool seen[200][201][201];
+
 double solve(int idx,int kk, int ll){
-    if(idx==n){return ll>=l&&kk>=200;}
+    // cout<<idx<<" "<<kk<<" "<<ll<<endl;
+    if(kk<0){return 0;}
+    if(idx==n){
+        return ll>=l;
+    }
     if(seen[idx][kk][ll]){return memo[idx][kk][ll];}
     double prob=p[idx];prob/=100;
     double ans=solve(idx+1,kk,ll)*(1-prob);
-    ans+=solve(idx+1,min(a[idx]+kk,400),ll+1)*prob;
+    if(a[idx]>0){
+        ans+=solve(idx+1,min(a[idx]+kk,200),ll+1)*prob;
+    }
+    if(a[idx]==-1&&kk){
+        ans+=solve(idx+1,kk-1,ll+1)*prob;
+    }
     seen[idx][kk][ll]=1;
     return memo[idx][kk][ll]=ans;
 }
 int main(){
     scanf("%d%d%d",&n,&l,&k);
+    for(int i=0;i<n;i++){scanf("%d",&p[i]);}
+    for(int i=0;i<n;i++){scanf("%d",&a[i]);}
     for(int i=0;i<n;i++){
-        scanf("%d",&p[i]);
+        for(int j=i+1;j<n;j++){
+            if(a[i]<a[j]){
+                swap(a[i],a[j]);
+                swap(p[i],p[j]);
+            }
+        }
     }
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    printf("%.6lf\n",solve(0,k+200,0));
+    printf("%.6lf\n",solve(0,k,0));
     return 0;
 }
