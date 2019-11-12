0687c7f5faa85ff4ef8b30e563dfdafe27ebce96
OmarAhmedSaleh <omar.sa006@gmail.com>
2019-11-12 13:04:45 +0200

    Create Breadth First Search: Shortest Reach.cpp

---
+++ HackerRank/Breadth First Search: Shortest Reach.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the bfs function below.
+vector<int> bfs(int n, int m, vector<vector<int>> edges, int s) {
+    vector < int > dis(n , -1);
+    vector < vector < int > > graph(n);
+    for(int i = 0;i < m ; i++){
+        int v = edges[i][0];
+        int u = edges[i][1];
+        graph[u - 1].push_back(v - 1);
+        graph[v - 1].push_back(u - 1);
+    }
+    dis[s - 1] = 0;
+    queue < int > q;
+    q.push(s - 1);
+    while(!q.empty()){
+        int idx = q.front();
+        q.pop();
+        int to = -1;
+        for(int i = 0;i < (int)graph[idx].size(); i++){
+            to = graph[idx][i];
+            if(dis[to] == -1 || dis[to] > dis[idx] + 6){
+                dis[to] = dis[idx] + 6;
+                q.push(to);
+            }
+        }
+    }
+    dis.erase(dis.begin() + (s - 1));
+    return dis;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string nm_temp;
+        getline(cin, nm_temp);
+
+        vector<string> nm = split_string(nm_temp);
+
+        int n = stoi(nm[0]);
+
+        int m = stoi(nm[1]);
+
+        vector<vector<int>> edges(m);
+        for (int i = 0; i < m; i++) {
+            edges[i].resize(2);
+
+            for (int j = 0; j < 2; j++) {
+                cin >> edges[i][j];
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        int s;
+        cin >> s;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        vector<int> result = bfs(n, m, edges, s);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
