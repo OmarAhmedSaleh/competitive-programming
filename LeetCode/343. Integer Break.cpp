703a484cfb55abb6aa6df90c2943546e082e1b70
OmarAhmedSaleh <omar.sa006@gmail.com>
2020-06-22 21:07:26 +0200

    Update 343. Integer Break.cpp

--- LeetCode/343. Integer Break.cpp
+++ LeetCode/343. Integer Break.cpp
@@ -3,7 +3,6 @@ public:
     int integerBreak(int n) {
       vector < int > dp(n + 1 , 0);
       dp[2] = 1;
-      dp[1] = 1;
       for(int i = 3 ; i <= n ; i++){
         for(int j = 1 ; j < i ; j++){
           dp[i] = max(dp[i] , j * dp[i - j]);
