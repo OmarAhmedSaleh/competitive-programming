62e8a9debb0cfb44558fb9a4bce540346dcdacd1
OmarAhmedSaleh <omarahmedsaleeh@gmail.com>
2021-02-24 19:05:45 +0200

    Create Score of Parentheses  Solution.cpp

---
+++ LeetCode/Score of Parentheses  Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        int sz = (int)S.size();
+        long long ans = 0;
+        vector<int> st;
+        vector<long long> cnt(sz, 0);
+        for(int i = 0;i < sz;i++){
+            if(S[i] == '('){st.push_back(i); continue;}
+            int idx = st.back(); st.pop_back();
+            int sc = cnt[idx] > 0 ? cnt[idx] * 2 : 1;
+            if(st.size() > 0){
+                cnt[st.back()] += sc;
+            }else{
+                ans += sc;
+            }
+        }
+        return ans;
+    }
+};
